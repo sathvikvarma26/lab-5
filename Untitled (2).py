d56d53c918a3aa0f5cf1e06be77968cec5d63b4d
sathvikvarma26 <143284935+sathvikvarma26@users.noreply.github.com>
2023-10-09 09:16:47 +0530

    Update Untitled (2).py

--- Untitled (2).py
+++ Untitled (2).py
@@ -637,6 +637,19 @@ mlp_project.fit(X_train, y_train)
 y_pred = mlp_project.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy on test data: {accuracy}")
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
+df=pd.read_excel("embeddingsdatasheet-1.xlsx")
+X = df.drop('Label',axis=1)
+y = df['Label']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+mlp_project = MLPClassifier(hidden_layer_sizes=(100, 50), activation='relu', solver='adam', max_iter=1000)
+mlp_project.fit(X_train, y_train)
+y_pred = mlp_project.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy on test data: {accuracy}")
 
 
 
